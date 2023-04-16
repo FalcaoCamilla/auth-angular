59594fac6d4cfea35dc1d6b7fa21d813cdb0afdd
FalcaoCamilla <millafalcao.acad@gmail.com>
2023-04-16 01:28:39 -0300

    logout implemented

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, catchError, map, throwError } from 'rxjs';
 
 @Injectable({
@@ -9,16 +10,29 @@ export class AuthService {
 
   private url: string = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
 
   public signin(payload: {email: string, password: string}): Observable<any>{
-    return this.http.post(`${this.url}/sign`, payload).pipe(
+    return this.http.post<{token: string}>(`${this.url}/sign`, payload).pipe(
       map(data => {
-        return console.log(data)
+        localStorage.removeItem('access_token');
+        localStorage.setItem('access_token', data.token);
+        return this.router.navigate(['admin']);
       }),
-      catchError(error => {
-        return throwError(() => error.error.message);
+      catchError(e => {
+        if(e.error.message){
+          return throwError(() => e.error.message);
+        }
+        return throwError(() => "Falha na validação")
       })
     )
+  }
+
+  public logout(){
+    localStorage.removeItem('acces_token');
+    this.router.navigate([''])
   }
 }
